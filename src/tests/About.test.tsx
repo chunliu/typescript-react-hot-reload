8f7c4ccf8ec6c706341713684ea286ab04f4829b
Chun Liu <chunliu@users.noreply.github.com>
2017-05-07 13:29:33 +0800

    Add test support with Jest and enzyme

---
+++ src/tests/About.test.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+// Jest snapshot test
+// import * as TestUtils from "react-dom/test-utils";
+// import AboutPage from "../components/about/AboutPage";
+
+// describe("<AboutPage />", () => {
+//     it("render", () => {
+//         expect(TestUtils.createRenderer().render(
+//             <AboutPage />
+//         )).toMatchSnapshot();
+//     });
+// });
+import {shallow} from "enzyme";
+import AboutPage from "../components/about/AboutPage";
+
+describe("<AboutPage />", () => {
+    it("should render without throwing an error", () => {
+        expect(shallow(<AboutPage />).contains(<h1>About</h1>)).toBe(true);
+    });
+});
