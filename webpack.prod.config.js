99d0aaccab9e543691ec8439ce0aac2d48657709
Chun Liu <chunliu@users.noreply.github.com>
2017-08-17 21:00:45 +0800

    Add build config for production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,57 +1,27 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
     context: resolve(__dirname, 'src'),
-    entry: [
-        'react-hot-loader/patch',
-        // activate HMR for React
-        'webpack-dev-server/client?http://localhost:8080',
-        // bundle the client for webpack-dev-server
-        // and connect to the provided endpoint
-        'webpack/hot/only-dev-server',
-        // bundle the client for hot reloading
-        // only- means to only hot reload for successful updates
-        './index.tsx'
-        // the entry point of our app
-    ],
+    entry: './index.tsx',
     output: {
         filename: 'hotloader.js',
         // the output bundle
         path: resolve(__dirname, 'dist'), 
-        publicPath: '/'
-        // necessary for HMR to know where to load the hot update chunks
+        libraryTarget: "umd",
+        library: "Hotloader",
     },
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
-    devServer: {
-        hot: true,
-        // enable HMR on the server
-        noInfo: true,
-        quiet: false,
-        // minimize the output to terminal.
-        contentBase: resolve(__dirname, 'src'),
-        // match the output path
-        publicPath: '/'
-        // match the output `publicPath`
-    },
     module: {
-        rules: [
-            {
-                enforce: "pre",                
-                test: /\.(ts|tsx)?$/, 
-                loader: 'tslint-loader',
-                exclude: [resolve(__dirname, "node_modules")],
-            },             
+        rules: [            
             { 
                 test: /\.(ts|tsx)?$/, 
                 use: [
-                    {loader: 'react-hot-loader/webpack'}, 
                     {loader: 'babel-loader'},
                     {loader: 'awesome-typescript-loader'}, 
                 ] 
@@ -68,12 +38,14 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        // enable HMR globally
         new webpack.NamedModulesPlugin(),
         // prints more readable module names in the browser console on HMR updates
         new HtmlWebpackPlugin({template: resolve(__dirname, 'src/index.html')}),
-        // inject <script> in html file. 
-        new OpenBrowserPlugin({url: 'http://localhost:8080'}),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
     ],
 };
