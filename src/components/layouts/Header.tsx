8859288503d66ec7f9239938f0235488225137d3
Chun Liu <chunliu@users.noreply.github.com>
2017-08-23 11:20:08 +0800

    change some components to be stateless

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,24 +4,18 @@ import { Layout, Row, Col, Menu, Icon } from "antd";
 import { Link } from 'react-router-dom'
 import "./Header.less";
 
-interface HeaderProps {};
-
-interface HeaderState {};
-
-export class Header extends React.Component<HeaderProps, HeaderState> {
-    public render(): JSX.Element {
-        return (
-            <Layout.Header style={{ background: '#fff', padding: 0 }}>
-                <Row type="flex" justify="end" align="middle">
-                    <Col span={3}>
-                        <Menu mode="horizontal" className="user-logout">
-                            <Menu.SubMenu title={<span><Icon type="user" />{"User 1"}</span>} >
-                                <Menu.Item key="logOut"><Link to="#" >Logout</Link></Menu.Item>
-                            </Menu.SubMenu>
-                        </Menu>  
-                    </Col>
-                </Row>
-            </Layout.Header>
-        );
-    }
-}
+export const Header: React.StatelessComponent<{}> = () => {
+    return (
+        <Layout.Header style={{ background: '#fff', padding: 0 }}>
+            <Row type="flex" justify="end" align="middle">
+                <Col span={3}>
+                    <Menu mode="horizontal" className="user-logout">
+                        <Menu.SubMenu title={<span><Icon type="user" />{"User 1"}</span>} >
+                            <Menu.Item key="logOut"><Link to="#" >Logout</Link></Menu.Item>
+                        </Menu.SubMenu>
+                    </Menu>  
+                </Col>
+            </Row>
+        </Layout.Header>
+    );
+};
--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,33 +1,25 @@
 import * as React from 'react';
 import { Layout } from 'antd';
 import Sidebar from './Sidebar';
-//import HomePage from "../home/HomePage";
 import { Header } from "./Header";
 import { Redirect } from "react-router-dom";
 import {renderRoutes} from "react-router-config";
 import { routes } from "../routes";
 import "./PageLayout.less";
 
-
-interface LayoutProps {};
-
-interface LayoutState {};
-
-class PageLayout extends React.Component<LayoutProps, LayoutState> {
-    public render(): JSX.Element {
-        return (
-            <Layout className="ant-layout-has-sider">
-                <Sidebar />
-                <Layout>
-                    <Layout.Content>
-                        <Header />
-                        <Redirect to="/home" /> 
-                        {renderRoutes(routes)}
-                    </Layout.Content>
-                </Layout>
+const PageLayout: React.StatelessComponent<{}> = () => {
+    return (
+        <Layout className="ant-layout-has-sider">
+            <Sidebar />
+            <Layout>
+                <Layout.Content>
+                    <Header />
+                    <Redirect to="/home" /> 
+                    {renderRoutes(routes)}
+                </Layout.Content>
             </Layout>
-        );
-    }
-}
+        </Layout>
+    );
+};
 
 export default PageLayout;
