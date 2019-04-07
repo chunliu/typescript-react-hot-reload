c970027f1a4017fd1b7eaec52a0d5b8ee735788c
Vinaya Vaman Nayak <pdnsjce@gmail.com>
2019-04-07 11:17:41 +0200

    check for HMR on index and replace the component on update

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,15 @@ ReactDOM.render(
     <App />,
     document.getElementById("app"),
 );
+
+
+// Hot Module Replacement
+declare let module: { hot: any };
+
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    const NewApp = require("./components/App").default;
+
+    ReactDOM.render(<NewApp />, document.getElementById('app'));
+  });
+}
