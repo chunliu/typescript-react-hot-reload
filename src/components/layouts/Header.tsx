e8839e0a7b0e002d71695d1e91d771a727a91865
Chun Liu <chunliu@users.noreply.github.com>
2020-05-02 20:18:04 +0800

    fix build errors

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const Header: React.StatelessComponent<{}> = () => {
     return (
         <Layout.Header style={{ background: "#fff", padding: 0 }}>
-            <Row type="flex" justify="end" align="middle">
+            <Row justify="end" align="middle">
                 <Col span={3}>
                     <Menu mode="horizontal">
                         <Menu.SubMenu title={<span><UserOutlined />{"User 1"}</span>} >
